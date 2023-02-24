eaa1b155af8f301649d19b27524bc542bd533770
Morumbi <zun0323@gmail.com>
2023-02-24 17:41:40 -0300

    add input form in old ways

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,26 @@
+import React, { useState } from 'react';
+
 function ToDoList() {
-  return null;
+  const [todo, setTodo] = useState('');
+  const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setTodo(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(todo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          value={todo}
+          onChange={onChangeHandler}
+          placeholder='Write a to do'
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
