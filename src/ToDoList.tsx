67e26ef3635c4031f6b6cac9b4e4f6bd12f1b540
Morumbi <zun0323@gmail.com>
2023-02-24 20:54:42 -0300

    delete old form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -52,27 +52,4 @@ function ToDoList() {
   );
 }
 
-/* function ToDoList() {
-  const [todo, setTodo] = useState('');
-  const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
-    setTodo(event.currentTarget.value);
-  };
-  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmitHandler}>
-        <input
-          value={todo}
-          onChange={onChangeHandler}
-          placeholder='Write a to do'
-        />
-        <button>Add</button>
-      </form>
-    </div>
-  );
-} */
-
 export default ToDoList;
